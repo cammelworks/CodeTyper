fa162636da7f0da45190be4411050fbcf0088382
Yamakatsu63 <yamakatsu1217.63@gmail.com>
2019-01-24 19:50:50 +0900

    update js

--- typing/typing.js
+++ typing/typing.js
@@ -4,33 +4,49 @@
   let currentLocation;
   let score;
   let miss;
-  let timer;
-  //let target = document.getElementById("target");
+  let target = document.getElementById("target");
   let scoreLabel = document.getElementById("score");
   let missLabel = document.getElementById("miss");
   let timerLabel = document.getElementById("timer");
+  let time;
   let timerId;
   let initWord;
   let spaceKeyPressed;
   let fileSelected;
+  let inputedText;
 
+  const inputed = document.createElement("span");
+  inputed.classList.add("inputed");
+  const cursor = document.createElement("span");
+  const text = document.createElement("span");
   //初期化
   function init(){
     initWord = "Press space bar to start";
+    currentWord = "";
+    inputedText = "";
     currentLocation = 0;
     score = 0;
     miss = 0;
-    timer = 100;
+    time = 30;
     spaceKeyPressed = false;
     start.innerText = initWord;
+    // document.typing.target.value = currentWord;
+    target.innerText = currentWord;
     scoreLabel.innerHTML = score;
     missLabel.innerHTML = miss;
-    timerLabel.innerHTML = timer;
+    timerLabel.innerHTML = time;
     fileSelected = false;
+    inputed.textContent = inputedText;
+    target.appendChild(inputed);
+    cursor.textContent = currentWord[currentLocation];
+    target.appendChild(cursor);
+    text.textContent = currentWord.substring(currentLocation);
+    target.appendChild(text);
+    target.classList.add("hidden");
   }
   init();
 
-  //ファイルを読み込む
+  //ファイルの読み込み
   let obj = document.getElementById("selfile");
   obj.addEventListener("change", function(evt) {
     let file = evt.target.files;
@@ -39,73 +55,100 @@
     reader.onload = function(ev){
       fileSelected = true;
       currentWord = reader.result
-      document.typing.target.value = currentWord;
+      // document.typing.target.value = currentWord;
+      // target.innerText = currentWord;
+      cursor.textContent = currentWord[currentLocation];
+      text.textContent = currentWord.substring(currentLocation + 1);
     }
   })
 
   //タイマーの設置
+  let cursorCount = 0;
   function updateTimer(){
     timerId = setTimeout(function() {
-      timer--;
-      timerLabel.innerHTML = timer;
-      if(timer <= 0){
+      time -= 0.1;
+      cursorCount++;
+      timerLabel.innerHTML = time.toFixed(1);
+      if(cursorCount == 5){
+        cursor.classList.toggle("cursor");
+        cursorCount = 0;
+      }
+      if (time <= 0) {
         let accuracy = (score + miss) === 0 ? "0.00" : ((score / (score + miss)) * 100).toFixed(2);
-        alert(score + " letters, " + miss + " miss! " + accuracy + " %acuracy");
+        alert("Time is up!\n" + score + " letters, " + miss + " miss! " + accuracy + " %acuracy");
         clearTimeout(timerId);
         init();
         return;
       }
       updateTimer();
-    }, 1000);
+    }, 100);
   }
 
-  //
-  // function setTarget() {
-  //   currentWord = words[Math.floor(Math.random() * words.length)];
-  //   target.innerText = currentWord;
-  //   console.log(currentWord);
-  //   currentLocation = 0;
-  // }
-
-  //クリックで開始するときの処理
-  // window.addEventListener('click', function() {
-  //   if(!isStarted){
-  //     isStarted = true;
-  //     setTarget();
-  //     updateTimer();
-  //   }
-  // })
-
   //タイピングゲーム中の処理
+  window.addEventListener("keydown", function(e) {
+    if(e.key === "Tab"){
+      e.preventDefault();
+    }
+    console.log((e.keyCode === 9) && (currentWord[currentLocation] === " ") &&
+     (currentWord[currentLocation+1] === " ") &&
+     (currentWord[currentLocation+2] === " ") &&
+     (currentWord[currentLocation+3] === " "));
+    if((e.keyCode === 9) && (currentWord[currentLocation] === " ") &&
+     (currentWord[currentLocation+1] === " ") &&
+     (currentWord[currentLocation+2] === " ") &&
+     (currentWord[currentLocation+3] === " ")){
+      currentLocation += 4;
+      inputedText += "    ";
+      // document.typing.target.value = placeholder + currentWord.substring(currentLocation);
+      // target.innerText = inputedText + currentWord.substring(currentLocation);
+      inputed.textContent = inputedText;
+      cursor.textContent = currentWord[currentLocation];
+      text.textContent = currentWord.substring(currentLocation+1);
+      score++;
+      scoreLabel.innerHTML = score;
+    }
+  })
   window.addEventListener('keypress', function(e) {
     if(!fileSelected){
       alert("ファイルを選択してください");
     }
-    //console.log(String.fromCharCode(e.keyCode));
+    //ゲーム開始
     if(!spaceKeyPressed) {
+      if(!fileSelected){
+        return;
+      }
       if(String.fromCharCode(e.keyCode) === " "){
         spaceKeyPressed = true;
         updateTimer();
         initWord = "";
         start.innerText = initWord;
+        target.classList.remove("hidden");
+        cursor.textContent = currentWord[currentLocation];
       }
       return;
     }
-    console.log(currentWord[currentLocation].charCodeAt(0));
-    if(String.fromCharCode(e.keyCode) === currentWord[currentLocation]){
+
+    if((String.fromCharCode(e.keyCode) === currentWord[currentLocation]) ||
+    (e.keyCode === 13 && currentWord[currentLocation].charCodeAt(0) === 10)){
       currentLocation++;
-      if(e.keyCode === 10 || e.keyCode === 13){
-        currentLocation++;
+      if(e.keyCode === 13){
+        inputedText += "\n";
       }
-      let placeholder = "";
-      for(let i = 0; i < currentLocation; i++){
-        placeholder += "_";
-      }
-      document.typing.target.value = placeholder + currentWord.substring(currentLocation);
+      // let placeholder = "";
+      // for(let i = 0; i < currentLocation; i++){
+      //   placeholder += "_";
+      // }
+      inputedText += String.fromCharCode(e.keyCode);
+      // document.typing.target.value = placeholder + currentWord.substring(currentLocation);
+      // target.innerText = inputedText + currentWord.substring(currentLocation);
+      inputed.textContent = inputedText;
+      cursor.textContent = currentWord[currentLocation];
+      text.textContent = currentWord.substring(currentLocation+1);
       score++;
       scoreLabel.innerHTML = score;
+      // 次のコードへ
       if(currentLocation === currentWord.length){
-        setTarget();
+        init();
       }
     }else {
       miss++;
