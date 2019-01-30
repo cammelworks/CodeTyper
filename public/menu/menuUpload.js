66732f81368e0f22b586029d5eff72e2701bc159
kugi <kugimania@gmail.com>
2019-01-30 12:36:28 +0900

    menuShowlist.js 実装

--- public/menu/menuUpload.js
+++ public/menu/menuUpload.js
@@ -6,32 +6,33 @@
     } else {
         alert('The File APIs are not fully supported in this browser.');
     }
+    //ファイル名（拡張子なし取得用変数）
+    var reg=/(.*)(?:\.([^.]+$))/;
     
     var storageRef = firebase.storage().ref();
-    var databaseRef = firebase.database().ref("/c");
     var inputFile = document.getElementById('selfile');
-    console.log(databaseRef.orderByValue());
     function upload2fire(ev) {
         var target = ev.target;
         var files = target.files;
-        console.log(files);
         //ここで名前付け
         for(var i = 0, f; f = files[i]; i++){
-            var fileRef  = storageRef.child(files[i].name+files[i].type);
-            //データベースに同じファイルがあるか判定する。
-            var checkRef = databaseRef.orderByValue().equalTo(files[i].name);
-            if(checkRef === null){
-                databaseRef.push({filename: files[i].name});
-                console.log('Uploaded to Database!');
-                console.log(checkRef);
-            }else{
-                console.log('That name already exists.');
-                //alertで表示させたい
-            }
-            
+            //storage用(拡張子ありファイル名)
+            var filesrc = files[i].name;
+            //database用(拡張子なしファイル名)
+            var filename = filesrc.match(reg)[1];
+            var fileRef  = storageRef.child(filesrc);
+            var databaseRef = firebase.database().ref("/c/"+filename);
+            //set()で常に上書き
+            //databaseへのアップロード
+            databaseRef.set({
+                filename: filesrc
+            });
+            console.log('Uploaded to Database!');
+            //storageへのアップロード
             fileRef.put(files[i]).then(function(snapshot) {
                 console.log('Uploaded to Firestore!');
             });
+            
         }
     }
     inputFile.addEventListener('change', upload2fire, false);
